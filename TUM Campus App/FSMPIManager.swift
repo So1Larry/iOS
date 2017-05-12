9c5f675cacd3017e6e5b48359abe959621b0dd8e
Tim <TG908@users.noreply.github.com>
2017-05-12 14:53:20 +0200

    added FSMPIManager

---
+++ TUM Campus App/FSMPIManager.swift
@@ -0,0 +1,37 @@
+//
+//  FSMPIManager.swift
+//  TUM Campus App
+//
+//  Created by Tim Gymnich on 12.05.17.
+//  Copyright © 2017 LS1 TUM. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SWXMLHash
+
+class FSMPIManager: Manager {
+    
+    var main: TumDataManager?
+    var requiresLogin: Bool {
+        return false
+    }
+    
+    required init(mainManager: TumDataManager) {
+        main = mainManager
+    }
+    
+    func fetchData(_ handler: @escaping ([DataElement]) -> ()) {
+        Alamofire.request(getURL()).responseString() { (response) in
+            if let data = response.result.value {
+                let parsedXML = SWXMLHash.parse(data)
+                
+            }
+        }
+    }
+    
+    func getURL() -> String {
+        return "https://mpi.fs.tum.de/feed/"
+    }
+}
+ 
